fb52be387909a28cbf3d91ae0c6a5e06dd8bb0ce
Muhammad Talal <muhammadtalal1033@gmail.com>
2026-07-11 05:55:59 +0500

    data_types

---
+++ 02_basics_data_types.js
@@ -0,0 +1,6 @@
+"use strict";   // Treat all js code as new version
+
+// alert(3+3)  // alert is not working because we are in node and alert work by using html + js or it work in developer mode of google search engine
+
+console.log(typeof null);   // result => object
+console.log(typeof undefined);  // result => undefined
