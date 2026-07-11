50fb93a7f294d94b6caf41b25887833481fdf6f5
Muhammad Talal <muhammadtalal1033@gmail.com>
2026-07-11 06:39:00 +0500

    datatype conversions

---
+++ 03_conversionOperations.js
@@ -0,0 +1,124 @@
+/*
+
+let value = "50xyz"
+
+let numberValue = Number(value)
+
+console.log(typeof numberValue)
+// When we convert value = "50xyz" to number the console.log(typeof numberValue) gives number (data type) but in actually there is not number present
+
+console.log(numberValue)
+// console.log(numberValue) => NaN
+
+*/
+
+
+
+/*
+
+let value1 = null
+
+console.log(value1)
+console.log(typeof value1)
+
+let numberValue = Number(value1)
+
+console.log(typeof numberValue)
+console.log(numberValue)
+
+// console.log(value1) => null
+// console.log(typeof value1) => object
+// console.log(typeof numberValue) => number
+// console.log(numberValue) => 0
+
+*/
+
+
+
+
+/*
+
+let value2 = undefined
+
+console.log(value2)
+console.log(typeof value2)
+
+let numberValue = Number(value2)
+
+console.log(typeof numberValue)
+console.log(numberValue)
+
+// console.log(value2) => undefined
+// console.log(typeof value2) => undefined
+// console.log(typeof numberValue) => number
+// console.log(numberValue) => NaN
+
+*/
+
+
+
+/*
+
+let name = "Talal"
+console.log(name)
+console.log(typeof name)
+
+let nameNumber = Number(name)
+console.log(typeof nameNumber)
+console.log(nameNumber)
+
+// console.log(name) => Talal
+// console.log(typeof name) => string
+// console.log(typeof nameNumber) => number
+// console.log(nameNumber) => NaN
+
+*/
+
+
+/*
+
+let value3 = "Talal"
+
+console.log(value3)
+console.log(typeof value3)
+
+let booleanValue = Boolean(value3)
+console.log(booleanValue)
+
+// console.log(value3) => Talal
+// console.log(typeof value3) => string
+// console.log(booleanValue) => true
+
+let value4 = ""
+
+console.log(value4)
+console.log(typeof value4)
+
+let booleanVal = Boolean(value4)
+console.log(booleanVal)
+
+// console.log(value4) => print nothing
+// console.log(typeof value4) => string
+// console.log(booleanVal) => false
+
+*/
+
+
+/*
+
+let num = 20
+
+console.log(num)
+console.log(typeof num)
+
+let stringNum = String(num)
+
+console.log(stringNum)
+console.log(typeof stringNum)
+
+// console.log(num) => 20
+// console.log(typeof number) => number
+// console.log(stringNum) => 20 (string)
+// console.log(typeof stringNum) => string
+
+*/
