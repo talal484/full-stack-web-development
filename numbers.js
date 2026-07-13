330eafa6581d10b9c5e55de68a8dfd9805c9b30b
Muhammad Talal <muhammadtalal1033@gmail.com>
2026-07-13 09:24:38 +0500

    Numbers

---
+++ numbers.js
@@ -0,0 +1,25 @@
+// const score = 400
+// console.log(score)
+
+
+
+// const balance = new Number(100)
+// console.log(balance)    // return => [Number: 100]
+// console.log(balance.toString())     // return => 100 (string)
+// console.log(balance.toString().length)     // return => 3
+// console.log(balance.toFixed(2))     // return 2 digits after decimal => 100.00
+
+
+
+
+// const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(5))     // for 123.8966, return => 123.99
+// console.log(otherNumber.toPrecision(3))     // for 123.8966 round off to 124, return => 124 
+// console.log(otherNumber.toPrecision(3))     // for 1123.8966, return => 1.12e+3
+
+
+
+
+// const value = 1000000
+// console.log(value.toLocaleString())     // return => 1,000,000
+// console.log(value.toLocaleString('en-PK'))      // return according to pakistan version
