4deb7d66492eca49ff002955903e3411677492ed
Muhammad Talal <muhammadtalal1033@gmail.com>
2026-07-13 06:08:42 +0500

    strings discussion

---
+++ strings.js
@@ -0,0 +1,60 @@
+/*
+
+
+const name = "Talal"
+const age = 20
+console.log(`My name is ${name}. My age is ${age}.`)
+
+
+*/
+
+
+
+
+/*
+
+// const gameName = new String("Roblox123")
+// console.log(gameName[0])    // retrun => R
+// console.log(gameName.__proto__)     // return => {}
+// console.log(gameName.length)    // return =-> 9
+// console.log(gameName.toUpperCase())  // return => ROBLOX123
+// console.log(gameName.charAt('8'))    // retrun => 3
+// console.log(gameName.indexOf('o'))      // return => 1
+
+
+
+
+// const newString = gameName.substring(2, 6)   // We can't give negative indexes with substring()
+// console.log(newString)      // retrun => blox
+
+
+
+
+// // const anotherString = gameName.slice(2, 6)
+// const anotherString = gameName.slice(-7, -3)
+// console.log(anotherString)      // return => blox
+
+
+
+
+// const newStringone = "    Talal      "
+// console.log(newStringone.trim())
+
+// `trim()` is a JavaScript string method that **returns a new string** after removing all **whitespace** (such as spaces, tabs `\t`, newlines `\n`, and carriage returns `\r`) from the **beginning and end** of the original string. It **does not remove whitespace from the middle** of the string, and it **does not modify the original string** because JavaScript strings are immutable. If the original string has no whitespace at the start or end, `trim()` still returns a **new string** containing the same text (essentially a copy). For example, `"   Hello World   ".trim()` returns `"Hello World"`, `"Hello   World".trim()` returns `"Hello   World"` (the spaces in the middle remain), and `"Hello".trim()` also returns `"Hello"` even though there was nothing to remove.
+
+
+
+
+// const url = "https://talal.com/talal%20malik"
+// console.log(url.replace('%20', '-'))        //return => https://talal.com/talal-malik
+// console.log(url.includes("talal"))      // retrun => true
+
+
+
+
+// const myUniversity = "University-of-Veterinary-and-Animal-Sciences"
+// console.log(myUniversity.split("-"))
+// //  retrun array of strings => [ 'University', 'of', 'Veterinary', 'and', 'Animal', 'Sciences' ]
+
+
+*/
